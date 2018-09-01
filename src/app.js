70f8f21b1b7b7d22a38f80603941397869e17763
Douglas Panhota <douglaspands@gmail.com>
2018-09-01 12:13:47 -0300

    Ajuste na indentação do app.js

--- src/app.js
+++ src/app.js
@@ -16,36 +16,36 @@ app.set('package', require('./package'));
 const logger = require('./middleware/express-log')(app);
 // Executando modulos sincronamente
 (async () => {
-  // Incluindo middleware do Express
-  require('./middleware/express-modules')(app);
-  // Inicializando banco de dados
-  await require('./middleware/express-mongodb')(app);
-  // Registrando APIs
-  const routes = await require('./middleware/express-register-routes')(app);
-  return routes;
+    // Incluindo middleware do Express
+    require('./middleware/express-modules')(app);
+    // Inicializando banco de dados
+    await require('./middleware/express-mongodb')(app);
+    // Registrando APIs
+    const routes = await require('./middleware/express-register-routes')(app);
+    return routes;
 })().then(({ rest, graphql }) => {
-  // Inicializando o servidor
-  const server = app.listen((process.env.PORT || 3000), () => {
-    let environment = process.env.NODE_ENV || 'develop';
-    // Log da inicialização do servidor
-    logger.info(`Executando "${name}@${version}" em http://localhost:${server.address().port} (${environment})`);
-    // Caso seja o ambiente de desenvolvimento, disponibilizar interface para teste do GraphQL
-    if (environment === 'develop') logger.info(`GraphQL IDE disponivel em http://localhost:${server.address().port}/graphql`);
-    // Lista todas as APIs REST encontradas
-    rest.forEach(route => logger.info(`REST registrado....: ${route.uri} [${route.method}]`));
-    // Lista todas as APIs GraphQL encontradas
-    graphql.forEach(service => logger.info(`GraphQL registrado.: ${service}`));
-    // Criando health-check
-    const server_health = app_health.listen(((parseInt(process.env.PORT) + 1) || 3001), () => {
-      logger.log({
-        level: 'info',
-        source: 'health-check',
-        message: `Rota de Health-Check em: http://localhost:${server_health.address().port}/check`
-      });
-      require('./middleware/express-health-check')(app, rest, graphql, server, app_health);
+    // Inicializando o servidor
+    const server = app.listen((process.env.PORT || 3000), () => {
+        let environment = process.env.NODE_ENV || 'develop';
+        // Log da inicialização do servidor
+        logger.info(`Executando "${name}@${version}" em http://localhost:${server.address().port} (${environment})`);
+        // Caso seja o ambiente de desenvolvimento, disponibilizar interface para teste do GraphQL
+        if (environment === 'develop') logger.info(`GraphQL IDE disponivel em http://localhost:${server.address().port}/graphql`);
+        // Lista todas as APIs REST encontradas
+        rest.forEach(route => logger.info(`REST registrado....: ${route.uri} [${route.method}]`));
+        // Lista todas as APIs GraphQL encontradas
+        graphql.forEach(service => logger.info(`GraphQL registrado.: ${service}`));
+        // Criando health-check
+        const server_health = app_health.listen(((parseInt(process.env.PORT) + 1) || 3001), () => {
+            logger.log({
+                level: 'info',
+                source: 'health-check',
+                message: `Rota de Health-Check em: http://localhost:${server_health.address().port}/check`
+            });
+            require('./middleware/express-health-check')(app, rest, graphql, server, app_health);
+        });
     });
-  });
 }).catch(error => {
-  logger.error(error.stack);
-  process.exit(1);
+    logger.error(error.stack);
+    process.exit(1);
 });
